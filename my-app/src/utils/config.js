f86e11682d29760de915238033228e823355682e
TadX09 <tadx_09@hotmail.com>
2024-02-14 15:27:04 -0600

    Clean Project

--- my-app/src/utils/config.js
+++ my-app/src/utils/config.js
@@ -1,4 +1,5 @@
 const config = {
+    //UrlApiProject:'https://web-exk5pbhpoonh.up-us-nyc1-1.apps.run-on-seenode.com/',//Produccion
     UrlApiProject: 'http://127.0.0.1:8000/',
     DebuggingMode: true,
     Vesion: "1.0.4",
