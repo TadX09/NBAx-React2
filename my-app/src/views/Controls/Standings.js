12ec4b37cc5b3f6f862d9783810750c0c9dde3a8
TadX09 <tadx_09@hotmail.com>
2024-03-13 19:24:47 -0600

    Fix Calibration

--- my-app/src/views/Controls/Standings.js
+++ my-app/src/views/Controls/Standings.js
@@ -4,7 +4,7 @@ import React from "react";
  * STANDINGS
  * @component
  */
-const TOTAL_MAX_POINTS = 232;
+const TOTAL_MAX_POINTS = 220;
 class Standings extends React.Component {
   constructor(props) {
     super(props);
@@ -23,19 +23,22 @@ class Standings extends React.Component {
       TP_Away:0,
       UnderTP:0,
       OverTP:0,
-      FinalResult:0
+      FinalResult:0,
+      DiffTP:0,
+      DiffHome:0,
+      DiffVisitor:0
     };
   }
 
   componentDidMount() {
-    let mediaPointsHome = (this.state.totalPointsHome + this.state.totalPointsReciveHome)/2;
+    let mediaPointsHome = this.state.totalPointsHome;// (this.state.totalPointsHome + this.state.totalPointsReciveHome)/2;
     
-    let mediaPointsAway = (this.state.totalPointsAway + this.state.totalPointsReciveAway)/2;
+    let mediaPointsAway = this.state.totalPointsAway;//(this.state.totalPointsAway + this.state.totalPointsReciveAway)/2;
   
      // Board
-     let TP = Math.round(Number(mediaPointsHome + mediaPointsAway).toFixed(2));
-     let TP_Home = Math.round(Number(mediaPointsHome).toFixed(2));
-     let TP_Away = Math.round(Number(mediaPointsAway).toFixed(2));
+     let TP = mediaPointsHome + mediaPointsAway - 6;
+     let TP_Home = mediaPointsHome;
+     let TP_Away =mediaPointsAway;
  
      let factor = Math.abs(TP_Home - TP_Away);
  
@@ -44,7 +47,11 @@ class Standings extends React.Component {
  
      let FinalResult  = this.props.resHome + this.props.resAway;
 
-    this.setState({TP,TP_Home,TP_Away,OverTP,UnderTP,FinalResult});
+     let DiffTP = TP - FinalResult;
+     let DiffHome = TP_Home - this.props.resHome;
+     let DiffVisitor = TP_Away - this.props.resAway;
+
+    this.setState({TP,TP_Home,TP_Away,OverTP,UnderTP,FinalResult,DiffTP,DiffHome,DiffVisitor});
   }
 
   render() {
@@ -111,7 +118,7 @@ class Standings extends React.Component {
       <table className="tg">
       <thead>
         <tr>
-          <th className="tg-a4lg">Total Points</th>
+          <th className="tg-a4lg">Total Points | Diff:{this.state.DiffTP}</th>
         </tr>
      
       </thead>
@@ -124,9 +131,9 @@ class Standings extends React.Component {
       </td>
     </tr>
     <tr>
-      <td className="tg-4hcd" style={{fontSize:"16px",fontWeight:"bold",display:"flex",justifyContent:"center"}}>
-      <tr>H:{this.state.TP_Home}&nbsp;</tr>
-      <tr>V:{this.state.TP_Away}&nbsp;</tr>
+      <td className="tg-4hcd" style={{fontSize:"13px",fontWeight:"bold",display:"flex",justifyContent:"center"}}>
+      <tr>H:{this.state.TP_Home}&nbsp;DH:{this.state.DiffHome}&nbsp;|&nbsp;</tr>
+      <tr>V:{this.state.TP_Away}&nbsp;DV:{this.state.DiffVisitor}</tr>
        </td>
       
     </tr>
