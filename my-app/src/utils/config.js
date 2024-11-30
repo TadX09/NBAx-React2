ee1c3833aa48f9cee127126106e3aae4c4013375
TadX09 <tadx_09@hotmail.com>
2024-11-29 20:42:16 -0600

    Update config.js
    Se actualiza ruta del Api

--- my-app/src/utils/config.js
+++ my-app/src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    //UrlApiProject:'https://web-exk5pbhpoonh.up-us-nyc1-1.apps.run-on-seenode.com/',//Produccion USA
-    UrlApiProject:'https://web-jh5j5b9vao5h.up-fi-hel1-1.apps.run-on-seenode.com/',// Prod Helsinki
+    UrlApiProject:'https://web-exk5pbhpoonh.up-de-fra1-k8s-1.apps.run-on-seenode.com/',//Produccion USA
+    //UrlApiProject:'https://web-jh5j5b9vao5h.up-fi-hel1-1.apps.run-on-seenode.com/',// Prod Helsinki
     //UrlApiProject: 'http://127.0.0.1:8000/',
     DebuggingMode: true,
     Vesion: "1.0.4",
